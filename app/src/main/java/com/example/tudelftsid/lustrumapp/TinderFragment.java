22452e3c04099e8205873aecd9245ed1c8123526
Rolf de Vries <RBdeVries@student.tudelft.nl>
2017-11-02 12:30:09 +0100

    Updated the tinder fragment, for a better view. Added buttons. Buttons have no function yet

--- app/src/main/java/com/example/tudelftsid/lustrumapp/TinderFragment.java
+++ app/src/main/java/com/example/tudelftsid/lustrumapp/TinderFragment.java
@@ -3,6 +3,7 @@ package com.example.tudelftsid.lustrumapp;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

441a7474ed6158092d06707027bd671cad79a60a
Jipje <J.A.C.Rietveld@student.tudelft.nl>
2017-11-02 13:45:59 +0100

    Continued work on the information page. Gala will become a sort template for the others.

---
+++ app/src/main/java/com/example/tudelftsid/lustrumapp/GalaInfoActivity.java
@@ -0,0 +1,33 @@
+package com.example.tudelftsid.lustrumapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class GalaInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gala_info);
+
+        ImageView backgroundImage = (ImageView) findViewById(R.id.galaBackground);
+        backgroundImage.setImageResource(R.drawable.gala_info);
+
+        final Button infoButton = (Button) findViewById(R.id.gala_info_button);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.lustrumvirgiel.nl/evenementen/gala/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+    }
+}

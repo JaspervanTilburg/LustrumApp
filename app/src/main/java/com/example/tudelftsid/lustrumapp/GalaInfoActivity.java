5a233d083a726f2516adcab71ca864515005f558
Jipje <J.A.C.Rietveld@student.tudelft.nl>
2017-11-02 15:23:02 +0100

    Change the colour of the statusbar when going to the info page. Still need to fix the App Bar

--- app/src/main/java/com/example/tudelftsid/lustrumapp/GalaInfoActivity.java
+++ app/src/main/java/com/example/tudelftsid/lustrumapp/GalaInfoActivity.java
@@ -1,19 +1,28 @@
 package com.example.tudelftsid.lustrumapp;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class GalaInfoActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("Gala");
+        Window galaWindow = this.getWindow();
+        galaWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        galaWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        galaWindow.setStatusBarColor(this.getResources().getColor(R.color.colorAccent));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gala_info);
 

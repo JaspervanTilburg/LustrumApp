4fbefdb4cb75fa75e19650c9411118ba02b8d300
Jasper <jaspert95@gmail.com>
2017-11-24 21:15:09 +0100

    Fix right date

--- app/src/main/java/com/example/tudelftsid/lustrumapp/Tinder/TinderMatchAdapter.java
+++ app/src/main/java/com/example/tudelftsid/lustrumapp/Tinder/TinderMatchAdapter.java
@@ -2,15 +2,21 @@ package com.example.tudelftsid.lustrumapp.Tinder;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.tudelftsid.lustrumapp.LustrumRestClient;
 import com.example.tudelftsid.lustrumapp.Profile;
 import com.example.tudelftsid.lustrumapp.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -50,17 +56,36 @@ public class TinderMatchAdapter extends BaseAdapter {
                     inflate(R.layout.tinder_match_list_item, parent, false);
         }
         Profile currentItem = (Profile) getItem(position);
+        String dateDifference = getDateDifference(currentItem.getMatchCreatedAt());
         TextView textViewItemName = (TextView) convertView.findViewById(R.id.matchNameTxt);
         TextView textViewCreatedAt = (TextView) convertView.findViewById(R.id.matchCreatedAtTxt);
         TextView textViewNumber = (TextView) convertView.findViewById(R.id.phoneTxt);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.matchImageView);
 
         textViewItemName.setTypeface(font);
         textViewCreatedAt.setTypeface(font);
         textViewNumber.setTypeface(font);
 
+        Glide.with(context).load(LustrumRestClient.BASE_URL + currentItem.getAvatar()).into(imageView);
         textViewItemName.setText(currentItem.getName() + " (" + currentItem.getAge() + ")");
-        textViewCreatedAt.setText(currentItem.getMatchCreatedAt());
+        textViewCreatedAt.setText(dateDifference);
         textViewNumber.setText("+" + currentItem.getPhone());
         return convertView;
+    }
+
+    private String getDateDifference(String date) {
+        String[] numbers = date.split("-");
+        Calendar thatDay = Calendar.getInstance();
+        thatDay.set(Calendar.DAY_OF_MONTH,Integer.parseInt(numbers[2].split("T")[0]));
+        thatDay.set(Calendar.MONTH,Integer.parseInt(numbers[1])-1);
+        thatDay.set(Calendar.YEAR, Integer.parseInt(numbers[0]));
+        Calendar today = Calendar.getInstance();
+        long diff = today.getTimeInMillis() - thatDay.getTimeInMillis();
+        long days = diff / (24 * 60 * 60 * 1000);
+        if (days < 30) {
+            return (days + " dagen geleden");
+        }
+        SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+        return (format1.format(thatDay.getTime()));
     }
 }

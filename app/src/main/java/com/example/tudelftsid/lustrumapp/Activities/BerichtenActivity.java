f8bf018fee26810f89c2efa78c136fd05cb05345
Jasper <jaspert95@gmail.com>
2017-11-24 21:24:43 +0100

    Nog geen berichten

--- app/src/main/java/com/example/tudelftsid/lustrumapp/Activities/BerichtenActivity.java
+++ app/src/main/java/com/example/tudelftsid/lustrumapp/Activities/BerichtenActivity.java
@@ -1,7 +1,9 @@
 package com.example.tudelftsid.lustrumapp.Activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.tudelftsid.lustrumapp.R;
 
@@ -11,5 +13,9 @@ public class BerichtenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berichten);
+
+        TextView title = (TextView) findViewById(R.id.nogGeenBerichten);
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/DIN_Alternate_Bold.ttf");
+        title.setTypeface(font);
     }
 }

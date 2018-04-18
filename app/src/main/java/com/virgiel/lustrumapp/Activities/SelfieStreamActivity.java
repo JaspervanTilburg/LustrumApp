75d330e4dde90efbd540f64f4df09f69e2d41fe3
Jasper <jaspert95@gmail.com>
2018-04-18 15:15:26 +0200

    Request camera permissions

--- app/src/main/java/com/virgiel/lustrumapp/Activities/SelfieStreamActivity.java
+++ app/src/main/java/com/virgiel/lustrumapp/Activities/SelfieStreamActivity.java
@@ -1,14 +1,23 @@
 package com.virgiel.lustrumapp.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.ResponseHandlerInterface;
 import com.virgiel.lustrumapp.LustrumRestClient;
 import com.virgiel.lustrumapp.R;
 import com.virgiel.lustrumapp.Selfie;
@@ -17,6 +26,7 @@ import com.virgiel.lustrumapp.Utils;
 
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -28,6 +38,8 @@ import cz.msebera.android.httpclient.Header;
 public class SelfieStreamActivity extends AppCompatActivity {
 
     private Typeface body_font;
+    private List<Selfie> selfies;
+    public static final int CAMERA_PERMISSION = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +47,12 @@ public class SelfieStreamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_selfie_stream);
         body_font = Typeface.createFromAsset(getAssets(), "fonts/DIN_Bold.ttf");
 
-        LustrumRestClient.getWithHeader("selfies", null, new JsonHttpResponseHandler() {
+        LustrumRestClient.getWithHeader("/selfies", null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 System.out.println("Selfies Retrieved: " + response);
 
-                List<Selfie> selfies = Utils.loadSelfies(response);
+                selfies = Utils.loadSelfies(response);
                 SelfieStreamAdapter adapter = new SelfieStreamAdapter(getApplicationContext(), selfies, body_font);
                 ListView itemsListView  = findViewById(R.id.selfieListView);
                 itemsListView.setAdapter(adapter);
@@ -55,10 +67,55 @@ public class SelfieStreamActivity extends AppCompatActivity {
         FloatingActionButton photoButton = (FloatingActionButton) findViewById(R.id.photoButton);
         photoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermission();
+                } else {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePictureIntent, CAMERA_PERMISSION);
+                }
             }
         });
+    }
+
+    public void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("Posting " + data.getExtras().get("data"));
+        LustrumRestClient.postSelfie(data.getData().getPath(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println("Selfie posted: " + response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String msg, Throwable throwable) {
+                System.out.println("Something went wrong " + statusCode + ", " + throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject msg) {
+                System.out.println("Something went wrong with selfie post " + msg);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
     }
 
 }

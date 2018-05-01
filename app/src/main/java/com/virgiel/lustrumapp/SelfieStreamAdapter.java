fbda8e7d2b2c20076def59f2a65271fec76e5a48
Jasper <jaspert95@gmail.com>
2018-05-01 14:41:23 +0200

    Implement likebutton

--- app/src/main/java/com/virgiel/lustrumapp/SelfieStreamAdapter.java
+++ app/src/main/java/com/virgiel/lustrumapp/SelfieStreamAdapter.java
@@ -1,20 +1,24 @@
 package com.virgiel.lustrumapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.mindorks.placeholderview.annotations.Layout;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import org.json.JSONObject;
+
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by TUDelft SID on 16-11-2017.
@@ -47,16 +51,77 @@ public class SelfieStreamAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(context).
                     inflate(R.layout.selfie_list_item, parent, false);
         }
 
         Selfie currentItem = (Selfie) getItem(position);
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.selfieImageView);
+        ImageView imageView = convertView.findViewById(R.id.selfieImageView);
+        final TextView likesTextView = convertView.findViewById(R.id.likesTextView);
+        final View convertViewFinal = convertView;
+        ImageButton selfieLikeButton = convertView.findViewById(R.id.selfieLikeButton);
+
+        selfieLikeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Selfie selfie = items.get(position);
+                if (selfie.isLikedByMe()) {
+                    postSelfieUnLike(selfie.getId());
+                    likesTextView.setText((selfie.getLikes() - 1) + "");
+                    likesTextView.setTextColor(Color.WHITE);
+                } else {
+                    postSelfieLike(selfie.getId());
+                    likesTextView.setText((selfie.getLikes() + 1) + "");
+                    likesTextView.setTextColor(convertViewFinal.getContext().getResources().getColor(R.color.lustrumBlue_Dark));
+                }
+            }
+        });
+
         Glide.with(context).load(LustrumRestClient.BASE_URL + currentItem.getImageURL()).into(imageView);
+        likesTextView.setText(currentItem.getLikes().toString());
+        if (currentItem.isLikedByMe()) {
+            likesTextView.setTextColor(convertView.getContext().getResources().getColor(R.color.lustrumBlue_Dark));
+        }
         return convertView;
 
+    }
+
+    public void postSelfieLike(int image_id) {
+        LustrumRestClient.getWithHeader("/selfies/" + image_id + "/like", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println("Selfie liked: " + response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String msg, Throwable throwable) {
+                System.out.println("Something went wrong with selfie like" + statusCode + ", " + msg + ", " + throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject msg) {
+                System.out.println("Something went wrong with selfie like " + msg);
+            }
+        });
+    }
+
+    public void postSelfieUnLike(int image_id) {
+        LustrumRestClient.getWithHeader("/selfies/" + image_id + "/unlike", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println("Selfie unliked: " + response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String msg, Throwable throwable) {
+                System.out.println("Something went wrong with selfie unlike" + statusCode + ", " + msg + ", " + throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject msg) {
+                System.out.println("Something went wrong with selfie unlike " + msg);
+            }
+        });
     }
 }
